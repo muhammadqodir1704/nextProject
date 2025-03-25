1e6b7237cdcb81d5918eb8dc3cb3d6323bf7d1c1
Muhammadqodir <goldenparen06@gmail.com>
2025-03-25 20:16:00 +0500

    Rote added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <p>navbar</p>
+        <div className="navbar">
+           <Link href={'/'}>Bosh sahifa</Link>
+           <Link href={'/about'}>Men haqimda </Link>
+           <Link href={'/contact'}>Bog'lanish</Link>
+        </div>
         {children}
       </body>
     </html>
