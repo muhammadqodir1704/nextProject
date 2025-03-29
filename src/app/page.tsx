ef6bd51dffc73c82eb48d64f80370bd2afd3d2f3
Muhammadqodir <goldenparen06@gmail.com>
2025-03-30 00:14:16 +0500

    fix: errors fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,10 @@
-import Head from 'next/head'
 import React from 'react'
-const page = () => {
+
+export default function HomePage() {
   return (
-    <>
-    <Head>
-      <title>proffdev</title>
-    </Head>
     <div>
       <h2>myPage</h2>
     </div>
-    </>
   )
 }
-export default page
  
--- src/component/navbar.tsx
+++ src/component/navbar.tsx
@@ -2,15 +2,14 @@
 
 import { useRouter } from "next/navigation";
 
-const router = useRouter();
-const navbar = () => {
+export default function Navbar() {
+  const router = useRouter();
+  
   return (
     <div className="navbar-1">
-    <button onClick={() => router.push("/")}>Bosh Sahifa</button>
-    <button onClick={() =>router.push("/about")}>Men Haqimda</button>
-    <button onClick={() => router.push("/contact")}>Bog'lanish</button>
-  </div>
+      <button onClick={() => router.push("/")}>Bosh Sahifa</button>
+      <button onClick={() => router.push("/about")}>Men Haqimda</button>
+      <button onClick={() => router.push("/contact")}>Bog'lanish</button>
+    </div>
   )
 }
-
-export default navbar
