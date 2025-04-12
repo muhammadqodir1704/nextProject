3a9f6831aa13bfe6573fa9d037954e27f623341e
Muhammadqodir <goldenparen06@gmail.com>
2025-04-12 15:58:57 +0500

    SSR tested

--- src/component/home.tsx
+++ src/component/home.tsx
@@ -1,24 +1,40 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+
 const Home = () => {
     const [posts, setPosts] = useState([]);
     const [loading, setLoading] = useState(true);
+
     useEffect(() => {
       const getData = async () => {
-        try{
+        try {
            const {data} = await axios.get("https://jsonplaceholder.typicode.com/posts");
-           console.log(data);
-        }
-        catch(err){
+           setPosts(data);
+        } catch(err) {
           console.log(err);
-        }finally{
-            setLoading(false);
+        } finally {
+          setLoading(false);
         }
       }
       getData();
-    },[])
-  return <Home />;
+    }, []);
+
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div>
+        <h1>Posts</h1>
+        {posts.map((post: any) => (
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
+    );
 };
 
 export default Home;
