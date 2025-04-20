4e4140d81f9e00fedfbe93dc47b15bd5fba1767f
Muhammadqodir <goldenparen06@gmail.com>
2025-04-20 16:50:29 +0500

    feat: dynamic routing tested

---
+++ src/component/posts.tsx
@@ -0,0 +1,28 @@
+import React, { FC } from 'react'
+import { PostType } from '@/interface'
+import Link from 'next/link'
+
+const Posts: FC<{ data:PostType[]}> = ({data}) => {
+  return (
+    <table border={1}>
+    <thead>
+      <tr>
+        <th>id</th>
+        <th>title</th>
+      </tr>
+    </thead>
+    <tbody>
+      {data.map((c) => (
+        <tr key={c.id}>
+          <td>{c.id}</td>
+          <td>
+            <Link href={`/posts/${c.id}`}>{c.title}</Link>
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+  )
+}
+
+export default Posts
